dd40e9173d2da472300af5680d8037f5ef93b49e
BabekBeylerli <135702876+BabekBeylerli@users.noreply.github.com>
2023-08-30 01:39:10 +0400

    test

---
+++ src/main/java/com/example/turbospringproject/model/CarSalonsDto.java
@@ -0,0 +1,23 @@
+package com.example.turbospringproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class CarSalonsDto {
+    private String tittle;
+    private String type;
+    private String description;
+    private String description1;
+    private String description2;
+    private String logo;
+    private String image;
+    private Integer productsCount;
+    private Integer viewNumber;
+    private String location;
+}
---
+++ src/main/java/com/example/turbospringproject/model/ProductsDto.java
@@ -0,0 +1,33 @@
+package com.example.turbospringproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class ProductsDto {
+    private String type;
+    private String banType;
+    private double march;
+    private String marchType;
+    private String color;
+    private double price;
+    private String priceType;
+    private Integer owners;
+    private String fuelType;
+    private String gear;
+    private String gearBox;
+    private Integer year;
+    private Integer engineVolume;
+    private Integer enginePower;
+    private String market;
+    private Integer seatsNumber;
+    private String vinCode;
+    private String description;
+    private Integer viewCount;
+    private String activeStatus;
+}
