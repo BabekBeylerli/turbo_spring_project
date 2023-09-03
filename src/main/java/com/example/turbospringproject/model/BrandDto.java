1fad5cd78b249a11fdb1043abcf0963d20629700
nmammadli1021 <138964426+nmammadli1021@users.noreply.github.com>
2023-09-03 21:31:23 +0400

    afafaf

--- src/main/java/com/example/turbospringproject/model/BrandDto.java
+++ src/main/java/com/example/turbospringproject/model/BrandDto.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
