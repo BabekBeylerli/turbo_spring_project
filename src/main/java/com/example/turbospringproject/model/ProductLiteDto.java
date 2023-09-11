9f9647f31fc539c472874ffd49169291c9f69089
nmammadli1021 <138964426+nmammadli1021@users.noreply.github.com>
2023-09-11 14:30:11 +0400

    afafaf

--- src/main/java/com/example/turbospringproject/model/ProductLiteDto.java
+++ src/main/java/com/example/turbospringproject/model/ProductLiteDto.java
@@ -4,9 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class ProductLiteDto {
+    private Double price;
+    private String priceType;
+    private Integer year;
+    private Double engineVolume;
+    private Double march;
+    private String marchType;
+    private LocalDateTime createdAt;
+    private String brandName;
+    private String modelName;
+    private String subModelName;
+    private String cityName;
+    private String picture;
 }
