5b66a0a942988bb59e1d134837bc3623bf37083b
BabekBeylerli <babek.beylerli@bk.ru>
2023-09-29 17:32:07 +0400

    test

---
+++ src/main/java/com/example/turbospringproject/model/SubModelFilterDto.java
@@ -0,0 +1,12 @@
+package com.example.turbospringproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubModelFilterDto {
+    private String name;
+}
